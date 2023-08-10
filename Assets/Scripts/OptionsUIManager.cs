c572a93e26573f8c598c6cad3e511c6a0ed4a82a
Joshua Pawlik <joshuapawlik12@gmail.com>
2023-08-09 23:31:49 -0500

    Add Resolution Dropdown and custom font

--- Assets/Scripts/OptionsUIManager.cs
+++ Assets/Scripts/OptionsUIManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class OptionsUIManager : MonoBehaviour
 {
@@ -10,6 +12,10 @@ public class OptionsUIManager : MonoBehaviour
     public Slider musicVolumeSlider;
 
     public Toggle fullScreenToggleButton;
+    public TMP_Dropdown resolutionDropdown;
+
+    private int currentResolutionIndex = 0;
+    private Resolution[] availableResolutions;
 
 
     private void Start()
@@ -42,7 +48,52 @@ public class OptionsUIManager : MonoBehaviour
             musicVolumeSlider.value = GameSettings.musicVolume * 100.0f; // Convert from [0,1] to [0,100]
         }
 
+        if (resolutionDropdown != null)
+        {
+            resolutionDropdown.ClearOptions();
+
+            availableResolutions = Screen.resolutions;  // Fetch the available resolutions
+
+            List<string> options = new List<string>();
+            HashSet<string> addedResolutions = new HashSet<string>();  // This set will help in filtering duplicates
+
+            for (int i = 0; i < availableResolutions.Length; i++)
+            {
+                string option = availableResolutions[i].width + "x" + availableResolutions[i].height;
+
+                // Only add the resolution if it hasn't been added before
+                if (!addedResolutions.Contains(option))
+                {
+                    options.Add(option);
+                    addedResolutions.Add(option);  // Mark this resolution as added
+
+                    // Check if it's the current resolution
+                    if (availableResolutions[i].width == Screen.currentResolution.width &&
+                        availableResolutions[i].height == Screen.currentResolution.height)
+                    {
+                        currentResolutionIndex = options.Count - 1;  // Assign the index based on the options list
+                    }
+                }
+            }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
+
+            resolutionDropdown.onValueChanged.AddListener(SetResolution);
+        }
+
+
+
     }
+    void SetResolution(int index)
+    {
+        Resolution newRes = availableResolutions[index];
+        Screen.SetResolution(newRes.width, newRes.height, Screen.fullScreen);
+        currentResolutionIndex = index;
+    }
+
+
 
     public void ToggleFullScreen()
     {
